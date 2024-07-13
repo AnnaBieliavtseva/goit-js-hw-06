9a6b9df759385ce2f2b91bf88b41551fbd12aafd
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2024-07-13 22:51:37 +0200

    Update task-11.js

--- js/task-11.js
+++ js/task-11.js
@@ -23,5 +23,103 @@ console.log(messageSplit)
 
 
   }
-console.log(slugify('How to become a JUNIOR developer for TWO WEEKS'))
+// console.log(slugify('How to become a JUNIOR developer for TWO WEEKS'))
 
+function calculateTotal(number) {
+  // Change code below this line
+   let total = 0;
+ for (i = 1; i <= number; i += 1) {
+   total += i;
+ }
+ 
+ return total;
+   // Change code above this line
+ }
+
+//  console.log(calculateTotal(5))
+
+
+
+ function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+for (let i = 0; i < order.length; i += 1) {
+  total += order[i];
+  // console.log(order[i])
+}
+
+  // Change code above this line
+  return total;
+}
+
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]))
+
+
+
+
+function findLongestWord(string) {
+  // Change code below this line
+let longestWord;
+let stringSplit = string.split(" ");
+let wordlength = 0;
+  for (let i = 0; i < stringSplit.length; i += 1) {
+    wordlength = stringSplit[1].length;
+    console.log(wordlength)
+    if (wordlength < stringSplit[i].length) {
+      longestWord = stringSplit[i];
+      return longestWord;
+    }
+}
+
+  // Change code above this line
+}
+
+
+// console.log(findLongestWord('May the force be with you'))
+
+
+function createArrayOfNumbers(min, max) {
+  const numbers = [];
+  // Change code below this line
+for (let i = min; i <= max; i += 1) {
+  numbers.push(i)
+}
+  // Change code above this line
+  return numbers;
+}
+
+// console.log(createArrayOfNumbers(1,5))
+
+
+
+function filterArray(numbers, value) {
+  // Change code below this line
+ let arr = []
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i])
+ if (numbers[i] > value) {
+   arr.push(numbers[i])
+ }
+}
+return arr;
+ // Change code above this line
+}
+
+console.log(filterArray([1,2,3,4,5,6,7], 4));
+
+
+function includes(array, value) {
+  // Change code below this line
+for (const signification of array){
+  if (signification === value){
+    return true;
+  } 
+  
+  
+} 
+return false;
+  // Change code above this line
+}
+
+console.log(includes(['apple', 'plum', 'pear', 'orange'], 'plum'));
+console.log(includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Uranus'))
