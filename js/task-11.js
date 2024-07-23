e532755a5d4320b8bc5c08e1ac7a2931e69f1f23
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2024-07-23 21:12:22 +0200

    Update task-11.js

--- js/task-11.js
+++ js/task-11.js
@@ -1,64 +1,67 @@
+// Task 1
+
 function calculateEngravingPrice(message, pricePerWord) {
-    // Change code below this line
+
  let messageSplit = message.split(" ");
  let countOfWords = messageSplit.length;
  let totalPrice = countOfWords * pricePerWord;
    return totalPrice;
    
 console.log(messageSplit)
-    // Change code above this line
+    
  }
 
 //  console.log(calculateEngravingPrice('Web-development is creative work', 40));
 //  console.log(calculateEngravingPrice('JavaScript is in my blood', 20));
 
 
- function slugify(title) {
+// Task 2 
+
+function slugify(title) {
  
 
    return  title.toLowerCase().split(" ").join("-");
-   
-
-
-
+  
 
   }
 // console.log(slugify('How to become a JUNIOR developer for TWO WEEKS'))
 
+
+// Task 3
 function calculateTotal(number) {
-  // Change code below this line
+
    let total = 0;
  for (i = 1; i <= number; i += 1) {
    total += i;
  }
  
  return total;
-   // Change code above this line
+   
  }
 
 //  console.log(calculateTotal(5))
 
 
-
+// Task 4
  function calculateTotalPrice(order) {
   let total = 0;
-  // Change code below this line
+  
 for (let i = 0; i < order.length; i += 1) {
   total += order[i];
   // console.log(order[i])
 }
 
-  // Change code above this line
+  
   return total;
 }
 
 // console.log(calculateTotalPrice([412, 371, 94, 63, 176]))
 
 
-
+// Task 5
 
 function findLongestWord(string) {
-  // Change code below this line
+  
 let longestWord;
 let stringSplit = string.split(" ");
 let wordlength = 0;
@@ -78,13 +81,15 @@ let wordlength = 0;
 // console.log(findLongestWord('May the force be with you'))
 
 
+// Task 6
+
 function createArrayOfNumbers(min, max) {
   const numbers = [];
-  // Change code below this line
+ 
 for (let i = min; i <= max; i += 1) {
   numbers.push(i)
 }
-  // Change code above this line
+  
   return numbers;
 }
 
@@ -92,8 +97,10 @@ for (let i = min; i <= max; i += 1) {
 
 
 
+// Task 7
+
 function filterArray(numbers, value) {
-  // Change code below this line
+ 
  let arr = []
 for (let i = 0; i < numbers.length; i += 1) {
   console.log(numbers[i])
@@ -102,14 +109,16 @@ for (let i = 0; i < numbers.length; i += 1) {
  }
 }
 return arr;
- // Change code above this line
+ 
 }
 
-console.log(filterArray([1,2,3,4,5,6,7], 4));
+// console.log(filterArray([1,2,3,4,5,6,7], 4));
 
+
+// Task 8
 
 function includes(array, value) {
-  // Change code below this line
+  
 for (const signification of array){
   if (signification === value){
     return true;
@@ -118,8 +127,164 @@ for (const signification of array){
   
 } 
 return false;
-  // Change code above this line
+
 }
 
-console.log(includes(['apple', 'plum', 'pear', 'orange'], 'plum'));
-console.log(includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Uranus'))
+// console.log(includes(['apple', 'plum', 'pear', 'orange'], 'plum'));
+// console.log(includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Uranus'))
+
+// Task 9
+
+function countProps(object) {
+  let propCount = 0;
+
+  let sum = [];
+for (const key in object) {
+  if (object.hasOwnProperty(key)) {
+    sum.push(key);
+   
+    propCount = sum.length;
+  }
+}
+ 
+  return propCount;
+}
+
+// console.log(countProps({ name: 'Mango', age: 2 }));
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+
+
+// Task 10
+
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+ 
+
+let values = Object.values(salaries);
+
+for (const value of values) {
+  totalSalary += value;
+}
+  
+  return totalSalary;
+}
+
+
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }))
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }))
+
+
+
+// Task 11
+
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+
+for (const color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
+}
+
+
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+
+
+// Task 12 
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+
+// function getProductPrice(productName) {
+ 
+// for (const product of products) {
+//   if (productName === product.name) {
+//     return product.price
+//   } 
+// }
+// return null;
+
+// }
+
+
+// console.log(getProductPrice("Radar"));
+// console.log(getProductPrice("Droid"));
+// console.log(getProductPrice("Engine"));
+
+// Task 13
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   let propValue = [];
+
+// for (const product of products) {
+//   const productKey = Object.keys(product);
+//   const productValue = Object.values(product);
+  
+//   for (i = 0; i < products.length; i += 1) {
+//     if (productKey[i] === propName) {
+//       propValue.push(productValue[i])
+//     }
+//   }
+// }
+// return propValue;
+
+// }
+
+
+// console.log(getAllPropValues("name"));
+// console.log(getAllPropValues("price"));
+// console.log(getAllPropValues("category"));
+
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+let total = 0;
+for (const product of products) {
+ 
+  const productValue = Object.values(product);
+
+  for (i = 0; i < products.length; i += 1) {
+   
+    if (productValue[i] === productName) {
+      // console.log(productValue[i].price)
+      total = product.price*product.quantity;
+    }
+  }
+}
+return total;
+
+}
+
+
+
+
+console.log(calculateTotalPrice("Blaster"));
+console.log(calculateTotalPrice("Radar"));
+console.log(calculateTotalPrice("Droid"));
