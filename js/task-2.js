290e3e40983f157c8ff95b309b6b531ddd9d73f6
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2024-12-16 22:05:00 +0100

    finish 6 hw

---
+++ js/task-2.js
@@ -0,0 +1,36 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    // for (let i = 0; i < this.#items.length; i++) {
+    //   if (itemToRemove === this.#items[i]) {
+    //     this.#items.splice(i, 1);
+    //   }
+    // }
+    this.#items.forEach((item, i, arr) => {
+      if (item === itemToRemove) {
+        arr.splice(i, 1);
+      }
+    });
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
